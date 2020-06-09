d92d5ff787db74bd2896f509bfff67926fbd4e72
Полина <polina110297@yandex.ru>
2020-06-09 15:58:55 +0300

    Get adverts count from props

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDom from "react-dom";
 import App from "./components/app/app.jsx";
 
+const Info = {
+  ADVERTS_COUNT: 152,
+};
+
 ReactDom.render(
-    <App />,
+    <App advertsCount={Info.ADVERTS_COUNT}/>,
     document.querySelector(`#root`)
 );
