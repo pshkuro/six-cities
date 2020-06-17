4d691a3aa4ae73fe868e00b2149ba6d74914ac91
Полина <polina110297@yandex.ru>
2020-06-17 11:54:55 +0300

    Create offers mocks

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+import App from "./components/app/app.jsx";
+import {offers} from "./mocks/offers.js";
 import React from "react";
 import ReactDom from "react-dom";
-import App from "./components/app/app.jsx";
 
 const AdvertsInfo = {
   advertsCount: 152,
   advertsDescription: [`Beautiful & luxurious apartment at great location`, `Wood and stone place`, `Beautyful seaview`, `Fantastic house with swimming pull`],
 };
+console.log(offers);
+
 
 ReactDom.render(
     <App advertsInfo={AdvertsInfo}/>,
---
+++ src/mocks/offers.js
@@ -0,0 +1,28 @@
+import {getRandomArrayItem, getRandomIntegerNumber, getRandomIntegerRoundingNumber} from "../utils/common.js";
+
+const OfferInfo = {
+  PICTURE: [`img/apartment-01.jpg`, `img/apartment-02.jpg`, `img/apartment-03.jpg`],
+  DESCRIPTION: [`Beautiful & luxurious apartment at great location`, `Wood and stone place`, `Beautyful seaview`, `Fantastic house with swimming pull`],
+  TYPE: [`Apartment`, `Room`, `House`, `Hotel`],
+};
+
+const generateOffer = () => {
+  return {
+    picture: getRandomArrayItem(OfferInfo.PICTURE),
+    premium: getRandomIntegerNumber(0, 1),
+    cost: getRandomIntegerNumber(50, 999),
+    description: getRandomArrayItem(OfferInfo.DESCRIPTION),
+    type: getRandomArrayItem(OfferInfo.TYPE),
+    rating: getRandomIntegerRoundingNumber(0, 5, 0),
+  };
+};
+
+const generateOffers = (count) => {
+  return new Array(count)
+  .fill(``)
+  .map(generateOffer);
+};
+
+export const offers = generateOffers(4);
+
+
