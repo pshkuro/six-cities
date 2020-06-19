c05441ee24bc6eb2d1e18c64f5a9c79ecd26b62e
Полина <polina110297@yandex.ru>
2020-06-19 10:56:59 +0300

    Add routing

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,39 @@
 import Main from "../main/main.jsx";
-import React from "react";
+import PlaceProperty from "../place-property/place-properrty.jsx";
 import PropTypes from "prop-types";
+import React, {PureComponent} from "react";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
+
 
 const advertCardTitleHandler = () => {};
 
-export default function App({offers}) {
-  return (
-    <Main offers={offers}
-      onAdvertCardTitleClick={advertCardTitleHandler}/>
-  );
+export default class App extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      step: -1,
+    };
+  }
+
+  render() {
+    const {offers} = this.props;
+
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Main offers={offers}
+              onAdvertCardTitleClick={advertCardTitleHandler}/>
+          </Route>
+          <Route exact path="/property">
+            <PlaceProperty/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+
 }
 
 App.propTypes = {
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     open: true,
     inline: true,
     port: 1337,
-    historyApiFallback: true
+    historyApiFallback: true,
   },
   module: {
     rules: [
