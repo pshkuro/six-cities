1ac48b06163f1a7e9f510c224b05d5ff780102da
Полина <polina110297@yandex.ru>
2020-06-19 16:44:52 +0300

    Update tests with new mocks

--- src/components/place-card/place-card.e2e.test.js
+++ src/components/place-card/place-card.e2e.test.js
@@ -4,12 +4,21 @@ import React from "react";
 
 const props = {
   offer: {
-    picture: `img/apartment-05.jpg`,
+    pictures: [`img/apartment-05.jpg`],
     premium: true,
     cost: 10,
-    description: `Good apartment`,
+    description: [`Good apartment`],
     type: `Apartment`,
     rating: 5,
+    title: `Place cool`,
+    bedrooms: 2,
+    guests: 10,
+    conveniences: [`Beautiful`],
+    owner: {
+      avatar: `img/avatar-angelina.jpg`,
+      name: `Clara`,
+      pro: false,
+    },
     id: 1212,
   },
   onAdvertCardTitleClick: jest.fn(),
--- src/components/place-card/place-card.test.js
+++ src/components/place-card/place-card.test.js
@@ -4,12 +4,21 @@ import PlaceCard from "./place-card.jsx";
 
 const props = {
   offer: {
-    picture: `img/apartment-05.jpg`,
+    pictures: [`img/apartment-05.jpg`],
     premium: true,
     cost: 10,
-    description: `Good apartment`,
+    description: [`Good apartment`],
     type: `Apartment`,
     rating: 5,
+    title: `Place cool`,
+    bedrooms: 2,
+    guests: 10,
+    conveniences: [`Beautiful`],
+    owner: {
+      avatar: `img/avatar-angelina.jpg`,
+      name: `Clara`,
+      pro: false,
+    },
     id: 909,
   },
   onAdvertCardTitleClick: jest.fn(),
