0a460e7da1804938e6b7bc6505123e511f027dac
Полина <polina110297@yandex.ru>
2020-07-07 17:47:07 +0300

    Add Api module

---
+++ src/api/api.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const Error = {
+  UNAUTHORIZED: 401
+};
+
+const TIMEOUT = 5000;
+
+export const createApi = (onUnauthorized) => {
+  const api = axios.create({
+    baseURL: `https://htmlacademy-react-3.appspot.com/six-cities`,
+    timeout: TIMEOUT,
+    withCredentials: true,
+  });
+
+  const onSuccess = (response) => {
+    return response;
+  };
+
+  const onFail = (err) => {
+    const {response} = err;
+
+    if (response.status === Error.UNAUTHORIZED) {
+      onUnauthorized();
+      throw err;
+    }
+
+    throw err;
+  };
+
+  api.interceptors.response.use(onSuccess, onFail);
+
+  return api;
+};
